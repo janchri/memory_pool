8c0261c0a29592354de4add16d1722e053db0159
janchri <jans.christoph@gmail.com>
2025-03-27 21:34:00 +0100

    Fix some types

--- src/main.c
+++ src/main.c
@@ -9,21 +9,21 @@ int main()
     memory_pool_t pool;
     memory_pool_init(&pool);
 
-    // Create a pool with block size 32 bytes and 10 blocks
+    // Create a pool with block size 32 bytes
     memory_pool_create(&pool, 32u);
 
     // Allocate a block from the pool
-    uint8_t *blocks[MEMORY_POOL_SIZE / 32];
-    for (int i = 0; i < MEMORY_POOL_SIZE / 32; i++)
+    uint8_t *blocks[MEMORY_POOL_SIZE / 32u];
+    for (uint32_t i = 0; i < MEMORY_POOL_SIZE / 32u; i++)
     {
         blocks[i] = (uint8_t *)memory_pool_allocate(&pool);
         memset(blocks[i], i + 1, 32u);
     }
 
     // Deallocate the block back to the pool
-    memory_pool_free(&pool, blocks[0]);
-    memory_pool_free(&pool, blocks[0]);
-    memory_pool_free(&pool, blocks[1]);
+    memory_pool_free(&pool, blocks[0u]);
+    memory_pool_free(&pool, blocks[0u]);
+    memory_pool_free(&pool, blocks[1u]);
 
     return 0;
 }
